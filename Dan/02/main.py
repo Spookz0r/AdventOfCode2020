7c65da59adeb839e0cf6e273df4f5d1d44c5288d
Dan Nguyen <dan.nguyen@xxl.se>
2020-12-02 11:07:19 +0100

    Dan 02B

--- Dan/02/main.py
+++ Dan/02/main.py
@@ -2,6 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath('../utils'))
 from fileUtils import read_input
+from operator import xor
 
 def extract_info(line):
     info = line.split(" ")
@@ -12,10 +13,17 @@ def extract_info(line):
         "occurence": [int(num) for num in info[0].split("-")]
     }
 
-def is_password_valid(info):
+def is_password_validA(info):
     letter_count = info["password"].count(info["letter"])
     start, end = info["occurence"]
     return start <= letter_count <= end
+
+def is_password_validB(info):
+    password = info["password"]
+    letter = info["letter"]
+    pos1, pos2 = info["occurence"]
+    return xor(password[pos1-1] == letter, password[pos2-1] == letter)
+
 
 def count_valid_passwords(results):
     return sum(results)
@@ -23,7 +31,7 @@ def count_valid_passwords(results):
 def main():
     content = read_input("input.txt")
     password_info_list = [extract_info(line) for line in content]
-    results = [is_password_valid(info) for info in password_info_list]
+    results = [is_password_validB(info) for info in password_info_list]
     num_valid_passwords = count_valid_passwords(results)
     print("Valid passwords: {}".format(num_valid_passwords))
     
