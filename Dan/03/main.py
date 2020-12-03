6bcba35cf3227f610ec01a71717f89a310e81774
Dan Nguyen <dan.nguyen@xxl.se>
2020-12-03 14:37:25 +0100

    Dan 03B

--- Dan/03/main.py
+++ Dan/03/main.py
@@ -4,6 +4,8 @@ import os
 sys.path.append(os.path.abspath('../utils'))
 from fileUtils import read_input
 
+import math
+
 def get_traverse_path(hor, ver, ver_max):
     x, y = (0,0)
     coordinates = [(x, y)]
@@ -15,24 +17,36 @@ def get_traverse_path(hor, ver, ver_max):
         coordinates.append((x, y))
     return coordinates
 
-def isTree(coordinate, map):
+def is_tree(coordinate, map):
     x, y = coordinate
     hor_max = len(map[y])
     x = x % (hor_max)
     return map[y][x % (hor_max)] == "#"
 
-def countTrees(results):
+def count_trees(results):
     return sum(results)
+
+def get_slope_trees(map, slope):
+    hor_slope, ver_slope = slope
+    ver_max = len(map)
+    path = get_traverse_path(hor_slope, ver_slope, ver_max)
+    results = [is_tree(coordinate, map) for coordinate in path]
+    treeCount = count_trees(results)
+    return treeCount
+    print("Tree count: {}".format(treeCount))
 
 def main():
     content = read_input("input.txt")
-    hor_slope = 3
-    ver_slope = 1
-    ver_max = len(content)
-    path = get_traverse_path(hor_slope, ver_slope, ver_max)
-    results = [isTree(coordinate, content) for coordinate in path]
-    treeCount = countTrees(results)
-    print("Tree count: {}".format(treeCount))
+    slopes = [
+        (1, 1),
+        (3, 1),
+        (5, 1),
+        (7, 1),
+        (1, 2)
+    ]
+    trees = [get_slope_trees(content, slope) for slope in slopes]
+    print(math.prod(trees))
+
 
 if __name__ == "__main__":
     main()
